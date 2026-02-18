676359e84f18dc086a421bc38515f8d47740556e
sami <saalisboas@gmail.com>
2026-02-18 17:20:43 -0300

    Caminhos para arquivos e loop de palavras
    Os caminhos para os arquivos pdf e csv
    agora são relativos,a partir  do diretório main.
    A filtragem por etapas agora é feita em loop, de forma a
    rodar o código apenas uma vez. A primeira filtragem corresponde
    aos sites de compartilhamento de dados/códigos
    e as subsequentes, palavras chave de disponibilidade nos
    artigos. São separadas pela frase "PRÒXIMA FILTRAGEM...".

--- code/searching.py
+++ code/searching.py
@@ -3,11 +3,9 @@ import pathlib
 import numpy as np
 from difflib import SequenceMatcher as sm
 
-words = ['github', 'zenodo'] 
-#words = ['data availability statement', 'reasonable request', 'available on request', 'available upon request']
-#words = ['data availability', 'code availability']
+
 sheet = np.loadtxt(
-    '/home/saalisboas/pub/data/br/articles-br.csv',
+    'data/br/articles-br.csv',
     dtype='U',
     delimiter=',',
     quotechar='"',
@@ -15,31 +13,31 @@ sheet = np.loadtxt(
 sheet_title_column = sheet[:, 3] #used for search in sheet
 #select the info column. 
 sheet_doi_column = sheet[:, 7] 
-sheet_year_column = sheet[:, 4]
-sheet_journal_column = sheet[:, 5]
-sheet_cited_column = sheet[:, 6]
 
-directory_with_pdfs = '/home/saalisboas/pub/data/br'
+directory_with_pdfs = 'data/br'
 archives = pathlib.Path(directory_with_pdfs)
 pdf_all = list(archives.glob('*.pdf'))
 
-for pdf_n in pdf_all:
-    doc = pdf.open(pdf_n)
-    found = False
-    for n in range(doc.page_count):
-        page = doc.load_page(n) #text extraction is only for pages
-        page_text = page.get_text().lower() #lower for comparison with words
-        for word in words:
-            if word in page_text:
-                article_title = doc.metadata['title'] #extracted for search in sheet
-                print(f'ARQUIVO: {pdf_n}') 
-                title_similarity = []
-                for title in sheet_title_column:
-                    title_similarity.append(sm(None, title, article_title).ratio() > 0.85) #0.85 is a quality estimation
-                for indice, trues in enumerate(title_similarity):
-                    if trues:
-                        print(f'Informações: Linha: {indice}, DOI: {sheet_doi_column[indice]}')
-                found = True
-                break 
-        if found:
-            break
+list = [['github', 'zenodo'], ['data availability statement', 'reasonable request', 'available on request', 'available upon request'], ['data availability', 'code availability']]
+for words in list:
+	for pdf_n in pdf_all:
+	    doc = pdf.open(pdf_n)
+	    found = False
+	    for n in range(doc.page_count):
+	        page = doc.load_page(n) #text extraction is only for pages
+	        page_text = page.get_text().lower() #lower for comparison with words
+	        for word in words:
+	            if word in page_text:
+	                article_title = doc.metadata['title'] #extracted for search in sheet
+	                print(f'ARQUIVO: {pdf_n}') 
+	                title_similarity = []
+	                for title in sheet_title_column:
+	                    title_similarity.append(sm(None, title, article_title).ratio() > 0.85) #0.85 is a quality estimation
+	                for indice, trues in enumerate(title_similarity):
+	                    if trues:
+	                        print(f'Informações: Linha: {indice}, DOI: {sheet_doi_column[indice]}')
+	                found = True
+	                break 
+	        if found:
+	            break
+	print('PROXIMA FILTRAGEM ...')
